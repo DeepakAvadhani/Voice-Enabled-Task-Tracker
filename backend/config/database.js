93f8219741afb876ca01d5c13da1aec92377d670
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-12-03 23:45:54 +0530

    [update] updated database config file with database url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: "127.0.0.1",
-  user: "postgres",
-  password: "admin",
-  database: "task_tracker",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   max: 10,
   idleTimeoutMillis: 30000,
 });
@@ -22,6 +21,6 @@ pool
     process.exit(1);
   });
 
-const query = (text, params) => pool.query(text, params); //
+const query = (text, params) => pool.query(text, params);
 
 module.exports = { pool, query };
